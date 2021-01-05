3fced5d3e9d7c711951685ce6e50f7163d6a72ef
sachinjagtap4141 <71549532+sachinjagtap4141@users.noreply.github.com>
2021-01-05 15:06:20 +0530

    Update GitHubCheckPractice.java

--- src/rough1/GitHubCheckPractice.java
+++ src/rough1/GitHubCheckPractice.java
@@ -14,6 +14,16 @@ public class GitHubCheckPractice {
 		
 		
 		
+		System.out.println("changes removed today");
+		
+		
+		
+		
+		
+		
+		System.out.println("changes removed today");
+		
+		
 		
 		
 	}
